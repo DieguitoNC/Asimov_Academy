f33951d22bc8d9a433f7fd5ab9b4c46fcf12ec6c
DieguitoNC <diegocatalann@gmail.com>
2025-08-05 22:59:51 -0300

    comecando_o_jogo_da_velha

---
+++ Python Starter/Projetos/jogo_da_velha/jogo_da_velha.py
@@ -0,0 +1,46 @@
+"""
+Determinar como que é feito para vencer
+
+
+"""
+
+class Tic_Tac_Toe:
+    
+    continue_const = 0
+    
+    def __init__(self):
+        self.game = [1,2,3,4,5,6,7,8,9]
+        self.user = []
+        self.computer = []
+    
+    def possibilitys_to_win(self):
+        if [1,2,3] in self.game:
+            return scoreboard + 1
+        elif [1,4,7] in self.game:
+            return scoreboard + 1
+        elif [7,8,9] in self.game:
+            return scoreboard + 1
+        elif [3,6,9] in self.game:
+            return scoreboard + 1
+        elif [1,5,9] in self.game:
+            return scoreboard + 1
+        elif [3,5,7] in self.game:
+            return scoreboard + 1
+        elif [2,5,8] in self.game:
+            return scoreboard + 1
+        elif [4,5,6] in self.game:
+            return scoreboard + 1
+        elif [7,8,9] in self.game:
+            return scoreboard + 1
+        #aqui sao todas as opcoes de vitoria
+        
+    
+    def run_game(self):
+        while continue_const = 0:
+            print("================================")
+            print("Bem-vindo ao jogo da velha")
+            print("================================")
+            print()
+            
+            
+        
